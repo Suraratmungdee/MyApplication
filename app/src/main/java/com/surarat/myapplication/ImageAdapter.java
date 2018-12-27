711b09813cc7b16dcd65dd5e69c7344cd77a8a05
Suraratmungdee <surarat77@gmail.com>
2018-12-27 11:39:10 +0700

    Initial commit

---
+++ app/src/main/java/com/surarat/myapplication/ImageAdapter.java
@@ -0,0 +1,46 @@
+package com.surarat.myapplication;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+
+public class ImageAdapter extends BaseAdapter{
+    private Context mContext;
+
+    public ImageAdapter(Context c){
+        mContext = c;
+    }
+    public int getCount(){
+        return mThumIds.length;
+    }
+    public Object getItem(int position) {
+        return null;
+    }
+    public long getItemId(int position) {
+        return 0;
+    }
+    public View getView(int position, View converView, ViewGroup parent) {
+        ImageView imageView;
+        if (converView == null){
+            imageView = new ImageView(mContext);
+            imageView.setLayoutParams(new GridView.LayoutParams(200,200));
+            imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+            imageView.setPadding(8,8,8,8);
+        }else {
+            imageView = (ImageView) converView;
+        }
+        imageView.setImageResource(mThumIds[position]);
+        return imageView;
+    }
+    private Integer [] mThumIds = {
+            R.drawable.sample_0,R.drawable.sample_1,
+            R.drawable.sample_2,R.drawable.sample_3,
+            R.drawable.sample_4,R.drawable.sample_5,
+            R.drawable.sample_6,R.drawable.sample_7,
+            R.drawable.sample_2,R.drawable.sample_5,
+            R.drawable.sample_7,R.drawable.sample_0,
+    };
+}
